779b90f3314b0dbfeca4d34c609a52f95be33d1a
Prasad <Prasad@FullReview.com>
2018-06-14 10:24:28 +0530

    registration final

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url
-from django.contrib import admin
-from django.contrib.auth import views
-
+from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
